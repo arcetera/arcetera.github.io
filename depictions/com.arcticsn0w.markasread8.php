608fb7576b2531f8024df73708240dd7b3dafb57
arcticsn0w <alevine@hhai.org>
2014-12-14 16:47:02 -0500

    attempt to use depictions
    I’m using the RCRepo depiction system. I’m not sure if this’ll work,
    but we can always find out :P

---
+++ depictions/com.arcticsn0w.markasread8.php
@@ -0,0 +1,12 @@
+<?php
+	$name="MarkAsRead7 (iOS 8)";
+	$description=array(
+		"MarkAsRead7 recompiled for iOS 8. I just compiled it; DjKira is the developer.");
+
+	$changelog=array(
+		"0.0.2-1-2", " - Add depiction"
+		"0.0.2-1-1", " - Initial recompilation");
+
+	$devText="DjKira's Twitter";
+	$devLink="https://twitter.com/DjKira_"
+?>
