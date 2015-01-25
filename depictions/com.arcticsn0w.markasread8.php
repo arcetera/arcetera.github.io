a6487e23abc695afbb933a398c5efb6a75ce2e76
Matt <info@mgates.me>
2015-01-25 09:57:13 -0500

    Fix depiction php

--- depictions/com.arcticsn0w.markasread8.php
+++ depictions/com.arcticsn0w.markasread8.php
@@ -1,12 +1,13 @@
 <?php
-	$name="MarkAsRead7 (iOS 8)";
-	$description=array(
-		"MarkAsRead7 recompiled for iOS 8. I just compiled it; DjKira is the developer.");
+$name = "MarkAsRead7 (iOS 8)";
+$description = array(
+	"MarkAsRead7 recompiled for iOS 8. I just compiled it; DjKira is the developer." );
 
-	$changelog=array(
-		"0.0.2-1-2", " - Add depiction"
-		"0.0.2-1-1", " - Initial recompilation");
+$changelog = array(
+	"0.0.2-1-2", " - Add depiction",
+	"0.0.2-1-1", " - Initial recompilation"
+);
 
-	$devText="DjKira's Twitter";
-	$devLink="https://twitter.com/DjKira_"
+$devText = "DjKira's Twitter";
+$devLink = "https://twitter.com/DjKira_"
 ?>
